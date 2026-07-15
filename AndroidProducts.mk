b17133e3cd7f0112b59d0418b27813893699ed14
jihad <jhaidh277@gmail.com>
2026-07-15 20:55:30 +0600

    Fix formatting in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/infinity_beryl.mk
 
-    COMMON_LUNCH_CHOICES := \
+COMMON_LUNCH_CHOICES := \
     infinity_beryl-bp2s-user \
     infinity_beryl-bp2s-userdebug \
-   infinity_beryl-bp2s-eng
+    infinity_beryl-bp2s-eng
