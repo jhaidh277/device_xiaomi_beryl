5f35052336e21dece26aa7a3e8ea8679521bc3ba
jihad <jhaidh277@gmail.com>
2026-07-15 20:27:13 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,7 @@
-#
-# Copyright (C) 2025 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryl.mk
+    $(LOCAL_DIR)/infinity_beryl.mk
 
     COMMON_LUNCH_CHOICES := \
-    lineage_beryl-bp2s-user \
-    lineage_beryl-bp2s-userdebug \
-    lineage_beryl-bp2s-eng
+    infinity_beryl-bp2s-user \
+    infinity_beryl-bp2s-userdebug \
+   infinity_beryl-bp2s-eng
